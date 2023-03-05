e10920a0f3e27e1b2910f2e3598cf7385cac4ff3
Kim Haapamaki <kimhaafi@gmail.com>
2023-03-05 21:03:18 +0100

    footer is not displayed above other content

--- website_app/src/Components/Footer.js
+++ website_app/src/Components/Footer.js
@@ -3,20 +3,22 @@ import '../css/footer.css';
 
 function Footer(props) {
   return (
-    <footer className="footer">
-      <div className="column">
-        <h2 style={{ color: props.textColor }} className='center-text'>About Our Company</h2>
-        <p style={{ color: props.textColor }} className='center-text'>We are a company that does awesome things.</p>
-        <br />
-        <div style={{ color: props.textColor }} className="copyright">
-          &copy; 2023 RootPi AB. All rights reserved.
+    <div className='footer-box'>
+      <footer className="footer">
+        <div className="column">
+          <h2 style={{ color: props.textColor }} className='center-text'>About Our Company</h2>
+          <p style={{ color: props.textColor }} className='center-text'>We are a company that does awesome things.</p>
+          <br />
+          <div style={{ color: props.textColor }} className="copyright">
+            &copy; 2023 RootPi AB. All rights reserved.
+          </div>
         </div>
-      </div>
-      <div className="column column-right">
-        <h2 style={{ color: props.textColor }} className='center-text'>Contact Us</h2>
-        <p style={{ color: props.textColor }} className='center-text'>Email: info@rootpi.xyz</p>
-      </div>
-    </footer>
+        <div className="column column-right">
+          <h2 style={{ color: props.textColor }} className='center-text'>Contact Us</h2>
+          <p style={{ color: props.textColor }} className='center-text'>Email: info@rootpi.xyz</p>
+        </div>
+      </footer>
+    </div>
   );
 }
 
