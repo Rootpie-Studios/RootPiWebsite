dafa8bf6f90a123091a8c549f1e1f5cfbdfc7016
Kim Haapamaki <kimhaafi@gmail.com>
2023-03-06 19:49:06 +0100

    working on home page

--- website_app/src/Pages/HomePage.js
+++ website_app/src/Pages/HomePage.js
@@ -11,35 +11,30 @@ import Instapply from '../images/Instapply.png';
 import Coder from '../images/Coder.png';
 import AI from '../images/AI.png';
 import {BsUnity} from 'react-icons/bs';
-import {FaReact, FaPhp, FaHtml5} from 'react-icons/fa';
-import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3} from 'react-icons/si';
+import {FaReact, FaPhp, FaHtml5, FaVuejs, FaJava} from 'react-icons/fa';
+import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3, SiFlutter, SiLaravel} from 'react-icons/si';
 
 
 function HomePage() {
   return (
     <div className='App'>
       <Header subtitle={<img className='image-container' src={logo} alt="" />} />
-      <main>
+      <main style={{marginBottom: "50vw"}}>
         <div className='grid-container-header' style={{marginTop: '100px', marginBottom: '100px'}}>
           <div>
-            <div className='title'>
-              <h2>This is us</h2>
-            </div>
-            <p>Welcome to RootPi, the premier provider of cutting-edge virtual reality applications, mobile apps, and consulting services. At RootPi, we specialize in developing immersive and innovative virtual reality experiences that are sure to captivate and engage your audience. Our team of experienced developers and designers will work with you to create a custom virtual environment that is tailored to your specific needs and goals.</p>
-            <br/>
-            <p>We also offer mobile app development services using Flutter, a powerful cross-platform framework developed by Google. Our talented team of developers can create high-quality, user-friendly mobile apps that work seamlessly on both iOS and Android devices, helping you reach a wider audience and achieve greater success.</p>
-            <br/>
-            <p>But at RootPi, we don't just stop at development. Our comprehensive consulting services are designed to help you bring your ideas to life and achieve your goals. Our experienced consultants will work closely with you to understand your needs and provide tailored solutions that meet your unique requirements.</p>
-            <br/>
-            <p>At RootPi, we are committed to providing exceptional service and exceeding our clients' expectations. Our team of experts is constantly exploring new technologies and techniques to ensure that we are always at the forefront of innovation. Whether you're looking to create an immersive virtual reality experience or a user-friendly mobile app, RootPi has the expertise and experience to deliver exceptional results.</p>
-            <br/>
-            <p>So why wait? Contact RootPi today and let us help you bring your ideas to life.</p>
+            <img className='title-image-container rounded-image' style={{marginLeft: "20%"}} src={VRDude} alt="" />
+            <img className='title-image-container rounded-image' style={{marginLeft: "40%", marginTop: "600px"}} src={AI} alt="" />
           </div>
-          <div></div>
           <div>
-            <img className='title-image-container rounded-image' style={{marginRight: "10%"}} src={Coder} alt="" />
-            <img className='title-image-container rounded-image' style={{marginLeft: "70%"}} src={VRDude} alt="" />
-            <img className='title-image-container rounded-image' style={{marginLeft: "10%"}} src={AI} alt="" />
+          <div className='title'>
+            <h1>This is us</h1>
+            </div>
+            <h3 style={{marginTop: "80px"}}>Welcome to RootPi, the premier provider of cutting-edge virtual reality applications, mobile apps, and consulting services. At RootPi, we specialize in developing immersive and innovative virtual reality experiences that are sure to captivate and engage your audience. Our team of experienced developers and designers will work with you to create a custom virtual environment that is tailored to your specific needs and goals.</h3>
+            <br/>
+            <h3>So why wait? Contact RootPi today and let us help you bring your ideas to life.</h3>
+          </div>
+          <div>
+            <img className='title-image-container rounded-image' style={{marginLeft: "20%", marginTop: "500px"}} src={Coder} alt="" />
           </div>
         </div>
 
@@ -99,19 +94,37 @@ function HomePage() {
         </div>
 
         <div class="grid-container2">
+          <h1>
+            <SiCsharp style={{marginBottom: '10px'}} />
+            <SiPython style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <FaJava style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <FaPhp style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <FaHtml5 style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <SiJavascript style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <SiDart style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <SiCss3 style={{marginBottom: '10px', marginLeft: '10px'}} />
+          </h1>
+          <h1>
+            <SiBlender style={{marginBottom: '10px'}} />
+            <BsUnity style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <SiFlutter style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <SiLaravel style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <FaVuejs style={{marginBottom: '10px', marginLeft: '10px'}} />
+            <FaReact style={{marginBottom: '10px', marginLeft: '10px'}} />
+          </h1>
+        </div>
+
+        <div class="grid-container2">
           <Card textColor="black" children={
             <div>
               <h2 style={{marginBottom: '10px', overflow: 'visible', whiteSpace: 'nowrap'}}>Coding languages</h2>
-              <h1>
-                <SiCsharp style={{marginBottom: '10px'}} />
-                <SiPython style={{marginBottom: '10px', marginLeft: '10px'}} />
-                <FaPhp style={{marginBottom: '10px', marginLeft: '10px'}} />
-                <FaHtml5 style={{marginBottom: '10px', marginLeft: '10px'}} />
-                <SiJavascript style={{marginBottom: '10px', marginLeft: '10px'}} />
-                <SiDart style={{marginBottom: '10px', marginLeft: '10px'}} />
-                <SiCss3 style={{marginBottom: '10px', marginLeft: '10px'}} />
-              </h1>
               <p>We are compentent in many coding languages each with their own benefit to allow us to be flexible and deliver products that meet the client requirements</p>
+            </div>
+          } />
+          <Card textColor="black" children={
+            <div>
+              <h2 style={{marginBottom: '10px', overflow: 'visible', whiteSpace: 'nowrap'}}>Frameworks</h2>
+              <p>From our previous projects we have used many different frameworks that allows us to get work done faster at a higher level</p>
             </div>
           } />
         </div>
@@ -121,7 +134,7 @@ function HomePage() {
   );
 }
 
-// import {FaReact, FaPhp, FaHtml5} from 'react-icons/fa';
-// import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3} from 'react-icons/si';
+// import {FaReact, FaPhp, FaHtml5, FaVuejs, FaJava} from 'react-icons/fa';
+// import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3, SiFlutter, SiLaravel} from 'react-icons/si';
 
 export default HomePage;
