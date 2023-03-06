d79193d4275a6b3d4ed074ebaabaccbe01da2187
Jesper <jesper.laurell1@gmail.com>
2023-03-06 09:51:04 +0100

    student page test change

--- website_app/src/Pages/StudentPage.js
+++ website_app/src/Pages/StudentPage.js
@@ -13,7 +13,7 @@ function StudentPage() {
       <main>
         <Card color="#696969" textColor="white" children={
           <div>
-            <h2>This is us</h2>
+            <h2>Student projects</h2>
             <p>RootPi is a dynamic company that specializes in developing virtual reality applications in Unity, creating mobile apps in Flutter, and providing comprehensive consulting services to help clients bring their ideas to life. </p>
             <br/>
             <p>With a team of experienced developers and consultants, RootPi is committed to providing innovative solutions that meet the unique needs and goals of its clients.</p>
