0941a357319c11397d62a564cc7668ef61e8e482
Kim Haapamaki <kimhaafi@gmail.com>
2023-03-06 12:33:54 +0100

    working on homepage

--- website_app/src/Pages/HomePage.js
+++ website_app/src/Pages/HomePage.js
@@ -5,6 +5,8 @@ import Card from '../Components/Card';
 import logo from '../images/LogoTextTransBlack.png';
 import '../css/homepage.css';
 import ropes from '../images/RootPiRopesAssetIconHQ.png';
+import VRDude from '../images/VRDude.png';
+import TraumaSim from '../images/TraumaSim.png';
 
 
 function HomePage() {
@@ -12,29 +14,51 @@ function HomePage() {
     <div className='App'>
       <Header subtitle={<img className='image-container' src={logo} alt="" />} />
       <main>
-        <Card color="#696969" textColor="white" children={
+        <div className='grid-container'>
           <div>
-            <h2>This is us</h2>
+            <div className='title'>
+              <h2>This is us</h2>
+            </div>
             <p>RootPi is a dynamic company that specializes in developing virtual reality applications in Unity, creating mobile apps in Flutter, and providing comprehensive consulting services to help clients bring their ideas to life. </p>
             <br/>
             <p>With a team of experienced developers and consultants, RootPi is committed to providing innovative solutions that meet the unique needs and goals of its clients.</p>
           </div>
-        } />
+          <div></div>
+          <div>
+            <img className='card-image-container rounded-image' src={VRDude} alt="" />
+          </div>
+        </div>
 
-        <div className='title'>
-          <h2>What we are working on right now</h2>
+        <div class="grid-container">
+
+          <div className='title'>
+            <h2>What we are working on right now</h2>
+          </div>
         </div>
 
         <div class="grid-container">
           <Card textColor="black" children={
             <div>
               <h2>Traumasim - VR application for medical students</h2>
+              <div className='grid-container'>
+                <div>
+                </div>
+                <div>
+                </div>
+                <img className='card-image-container rounded-image' src={TraumaSim} alt="" />
+              </div>
             </div>
           } />
           <Card textColor="black" children={
             <div>
               <h2>Ropes - Unity asset store</h2>
-              <img className='card-image-container rounded-image' src={ropes} alt="" />
+              <div className='grid-container'>
+                <div>
+                </div>
+                <div>
+                </div>
+                <img className='card-image-container rounded-image' src={ropes} alt="" />
+              </div>
             </div>
           } />
           <Card textColor="black" children={
