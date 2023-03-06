851df520374c1b30af9b9f13fe95812ed3a43cd7
Kim Haapamaki <kimhaafi@gmail.com>
2023-03-06 18:56:07 +0100

    wokring on homepage

--- website_app/src/Pages/HomePage.js
+++ website_app/src/Pages/HomePage.js
@@ -8,8 +8,11 @@ import ropes from '../images/RootPiRopesAssetIconHQ.png';
 import VRDude from '../images/VRDude.png';
 import TraumaSim from '../images/TraumaSim.png';
 import Instapply from '../images/Instapply.png';
+import Coder from '../images/Coder.png';
+import AI from '../images/AI.png';
 import {BsUnity} from 'react-icons/bs';
-import {FaReact, FaPython} from 'react-icons/fa';
+import {FaReact, FaPhp, FaHtml5} from 'react-icons/fa';
+import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3} from 'react-icons/si';
 
 
 function HomePage() {
@@ -22,19 +25,21 @@ function HomePage() {
             <div className='title'>
               <h2>This is us</h2>
             </div>
-            <p>RootPi is a dynamic company that is at the forefront of cutting-edge technologies. The company specializes in developing immersive virtual reality applications using Unity, a popular game engine that has been adapted for VR applications. The team at RootPi is highly skilled in creating stunning and realistic virtual environments that can be used for a variety of applications, including gaming, training simulations, and more.</p>
+            <p>Welcome to RootPi, the premier provider of cutting-edge virtual reality applications, mobile apps, and consulting services. At RootPi, we specialize in developing immersive and innovative virtual reality experiences that are sure to captivate and engage your audience. Our team of experienced developers and designers will work with you to create a custom virtual environment that is tailored to your specific needs and goals.</p>
             <br/>
-            <p>In addition to their expertise in virtual reality development, RootPi also has a strong focus on mobile app development using Flutter, a cross-platform framework developed by Google. This allows them to create high-quality mobile apps that work seamlessly on both iOS and Android devices, providing clients with a wider reach and a greater potential audience.</p>
+            <p>We also offer mobile app development services using Flutter, a powerful cross-platform framework developed by Google. Our talented team of developers can create high-quality, user-friendly mobile apps that work seamlessly on both iOS and Android devices, helping you reach a wider audience and achieve greater success.</p>
             <br/>
-            <p>But RootPi doesn't just stop at development. They also offer comprehensive consulting services to help clients bring their ideas to life. Their team of experienced developers and consultants works closely with clients to understand their needs and goals, and then provides tailored solutions to help them achieve success. Whether it's developing a new app, optimizing an existing one, or providing strategic guidance, RootPi is committed to helping clients succeed.</p>
+            <p>But at RootPi, we don't just stop at development. Our comprehensive consulting services are designed to help you bring your ideas to life and achieve your goals. Our experienced consultants will work closely with you to understand your needs and provide tailored solutions that meet your unique requirements.</p>
             <br/>
-            <p>With a strong focus on innovation, RootPi is constantly exploring new technologies and techniques to provide the best possible solutions to their clients. Whether it's through virtual reality, mobile app development, or consulting services, RootPi is dedicated to delivering exceptional results and exceeding their clients' expectations.</p>
+            <p>At RootPi, we are committed to providing exceptional service and exceeding our clients' expectations. Our team of experts is constantly exploring new technologies and techniques to ensure that we are always at the forefront of innovation. Whether you're looking to create an immersive virtual reality experience or a user-friendly mobile app, RootPi has the expertise and experience to deliver exceptional results.</p>
+            <br/>
+            <p>So why wait? Contact RootPi today and let us help you bring your ideas to life.</p>
           </div>
           <div></div>
           <div>
-            <img className='title-image-container rounded-image' style={{marginRight: "10%"}} src={VRDude} alt="" />
+            <img className='title-image-container rounded-image' style={{marginRight: "10%"}} src={Coder} alt="" />
             <img className='title-image-container rounded-image' style={{marginLeft: "70%"}} src={VRDude} alt="" />
-            <img className='title-image-container rounded-image' style={{marginLeft: "10%"}} src={VRDude} alt="" />
+            <img className='title-image-container rounded-image' style={{marginLeft: "10%"}} src={AI} alt="" />
           </div>
         </div>
 
@@ -51,6 +56,8 @@ function HomePage() {
               <div className='grid-container-inner'>
                 <div style={{gridColumn: '1 / span 2'}}>
                   <BsUnity style={{marginBottom: '10px'}} />
+                  <SiBlender style={{marginBottom: '10px', marginLeft: '10px'}} />
+                  <SiCsharp style={{marginBottom: '10px', marginLeft: '10px'}} />
                   <p>We are working for Umeå university to build a virutal reality trauma simulation for students to train on</p>
                 </div>
                 <img className='card-image-container rounded-image' src={TraumaSim} alt="" />
@@ -63,6 +70,7 @@ function HomePage() {
               <div className='grid-container-inner'>
                 <div style={{gridColumn: '1 / span 2'}}>
                   <BsUnity style={{marginBottom: '10px'}} />
+                  <SiCsharp style={{marginBottom: '10px', marginLeft: '10px'}} />
                   <p>We are developing and maintaining an asset for Unity which simulates the physics of a rope</p>
                 </div>
                 <img className='card-image-container rounded-image' src={ropes} alt="" />
@@ -75,11 +83,35 @@ function HomePage() {
               <div className='grid-container-inner'>
                 <div style={{gridColumn: '1 / span 2'}}>
                   <FaReact style={{marginBottom: '10px'}} />
-                  <FaPython style={{marginBottom: '10px', marginLeft: '10px'}} />
+                  <SiPython style={{marginBottom: '10px', marginLeft: '10px'}} />
                   <p>A website for effortless generation of personalized cover letter, written for the work that you are applying to</p>
                 </div>
                 <img className='card-image-container rounded-image' src={Instapply} alt="" />
               </div>
+            </div>
+          } />
+        </div>
+
+        <div class="grid-container">
+          <div className='title'>
+            <h2>Our tech stack</h2>
+          </div>
+        </div>
+
+        <div class="grid-container2">
+          <Card textColor="black" children={
+            <div>
+              <h2 style={{marginBottom: '10px', overflow: 'visible', whiteSpace: 'nowrap'}}>Coding languages</h2>
+              <h1>
+                <SiCsharp style={{marginBottom: '10px'}} />
+                <SiPython style={{marginBottom: '10px', marginLeft: '10px'}} />
+                <FaPhp style={{marginBottom: '10px', marginLeft: '10px'}} />
+                <FaHtml5 style={{marginBottom: '10px', marginLeft: '10px'}} />
+                <SiJavascript style={{marginBottom: '10px', marginLeft: '10px'}} />
+                <SiDart style={{marginBottom: '10px', marginLeft: '10px'}} />
+                <SiCss3 style={{marginBottom: '10px', marginLeft: '10px'}} />
+              </h1>
+              <p>We are compentent in many coding languages each with their own benefit to allow us to be flexible and deliver products that meet the client requirements</p>
             </div>
           } />
         </div>
@@ -88,5 +120,8 @@ function HomePage() {
     </div>
   );
 }
+
+// import {FaReact, FaPhp, FaHtml5} from 'react-icons/fa';
+// import {SiCsharp, SiBlender, SiJavascript, SiDart, SiPython, SiCss3} from 'react-icons/si';
 
 export default HomePage;
